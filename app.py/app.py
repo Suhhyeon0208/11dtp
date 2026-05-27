da36a13e3e9ad1574f9e2e6d80ff0b26ecceea40
Suhhyeon0208 <24566@burnside.school.nz>
2026-05-28 09:24:07 +1200

    save

--- app.py/app.py
+++ app.py/app.py
@@ -1,9 +1,25 @@
+#docstring- Suhhyeon- airb=plane database application
+#imports
 import sqlite3
 
-db = sqlite3.connect("task7.db")
-cursor = db.cursor()
-sql = "SELECT * from task7;"
-cursor.execute(sql)
-result = cursor.fetchall()
-print(results)
-db.close()
+#contants and variables
+DATABASE = 'task7.db'
+
+#functions
+def print_all_aircraft():
+    """Print all the aircraft nicely"""
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM aircraft;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through all the results
+    for aircraft in results:
+        print(f"{aircraft[1]} {aircraft[2]} {aircraft[3]} {aircraft[4]}{aircraft[5]} {aircraft[6]}{aircraft[7]}")
+    #loop finished here
+    db.close()
+
+
+
+#main code
+print_all_aircraft()
--- test/test.py
+++ test/test.py
@@ -1,10 +1,25 @@
+#docstring- Suhhyeon- airb=plane database application
+#imports
 import sqlite3
 
-db = sqlite3.connect('cars.db')
-cursor = db.cursor()
-sql = "SELECT * FROM car;"
-cursor.execute(sql)
-results = cursor.fetchall()
-print(results)
-#close the db
-db.close()
+#contants and variables
+DATABASE = 'task7.db'
+
+#functions
+def print_all_aircraft():
+    """Print all the aircraft nicely"""
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM aircraft;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through all the results
+    for aircraft in results:
+        print(aircraft)
+    #loop finished here
+    db.close()
+
+
+
+#main code
+print_all_aircraft()
